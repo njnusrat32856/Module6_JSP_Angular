42230e2096b4b91d40132aeed0f11cc79e802619
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-06-30 19:02:56 +0600

    first jsp  class

---
+++ FirstJSPProgram/src/java/dao/StudentDao.java
@@ -0,0 +1,47 @@
+
+package dao;
+
+import dbutil.DBUtil;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Student;
+
+public class StudentDao {
+    
+    static Student s= new Student();
+
+    static PreparedStatement ps;
+    static ResultSet rs;
+    static String sql = "";
+    
+    public static int saveStudent(){
+    
+        int status = 0;
+        
+        sql = "insert into student(name,email,address,cell) values(?,?,?,?)";
+        
+        try {
+            ps = DBUtil.getConnect().prepareStatement(sql);
+            
+            ps.setString(1, s.getName());
+            ps.setString(2, s.getEmail());
+            ps.setString(3, s.getAddress());
+            ps.setString(4, s.getCell());
+            
+            status= ps.executeUpdate();
+            System.out.println(status);
+            
+            ps.close();
+            DBUtil.getConnect().close();
+            
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return status;
+    }
+    
+}
---
+++ FirstJSPProgram/web/home.jsp
@@ -0,0 +1,25 @@
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body style="color: aliceblue; background-color: cornflowerblue">
+        
+        <%= new java.util.Date() %>
+        
+        <h1 style="text-align: center">Hello World!</h1>
+        <h2>Welcome to JSP</h2>
+        
+        <p>
+            In Java, JSP stands for Jakarta Server Pages( (JSP; formerly JavaServer Pages)).
+            It is a server-side technology which is used for creating web applications.
+            It is used to create dynamic web content. JSP consists of both HTML tags and JSP tags.
+            In this, JSP tags are used to insert JAVA code into HTML pages.
+        </p>
+        
+        <h3>Nusrat Jahan</h3>
+    </body>
+</html>
