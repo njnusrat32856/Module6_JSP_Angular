91fe5fb89f189408dcaf4634f21fb66b4a84afe4
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-07-06 18:49:16 +0600

    JSF

---
+++ FirstJSFProgram/src/java/entity/Student.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student implements java.io.Serializable{
+    
+    private Integer id;
+     private String name;
+     private String email;
+     private String address;
+     private String cellno;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, String address, String cellno) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.cellno = cellno;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCellno() {
+        return cellno;
+    }
+
+    public void setCellno(String cellno) {
+        this.cellno = cellno;
+    }
+
+    
+     
+}
