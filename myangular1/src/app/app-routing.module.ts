6d3cf05b7fba5bc21d0a6f022a2821f1c55561ce
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-07-10 11:44:03 +0600

    create & delete

---
+++ myangular1/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+
+const routes: Routes = [
+  {path: 'student', component:StudentComponent},
+  {path: 'location', component:LocationComponent},
+  {path: 'createlocation', component:CreatelocationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
