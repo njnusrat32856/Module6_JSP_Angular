3a8c634bbe489605bf96edd98aa4be6483e3c696
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-07-14 18:40:04 +0600

    new Student

--- myangular1/src/app/app-routing.module.ts
+++ myangular1/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentComponent } from './student/student.component';
+
 import { LocationComponent } from './location/location.component';
 import { CreatelocationComponent } from './createlocation/createlocation.component';
 import { UpdatelocationComponent } from './updatelocation/updatelocation.component';
 
 const routes: Routes = [
-  {path: 'student', component:StudentComponent},
+  
   {path: 'location', component:LocationComponent},
   {path: 'createlocation', component:CreatelocationComponent},
   {path: 'updatelocation/:id', component:UpdatelocationComponent}
--- myangular1/src/app/app.module.ts
+++ myangular1/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
+
 import { LocationComponent } from './location/location.component';
 import { CreatelocationComponent } from './createlocation/createlocation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { UpdatelocationComponent } from './updatelocation/updatelocation.component';
+import { CreatestudentComponent } from './student/createstudent/createstudent.component';
+import { UpdatestudentComponent } from './student/updatestudent/updatestudent.component';
+import { ViewstudentComponent } from './student/viewstudent/viewstudent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent,
+    
     LocationComponent,
     CreatelocationComponent,
-    UpdatelocationComponent
+    UpdatelocationComponent,
+    CreatestudentComponent,
+    UpdatestudentComponent,
+    ViewstudentComponent
   ],
   imports: [
     BrowserModule,
